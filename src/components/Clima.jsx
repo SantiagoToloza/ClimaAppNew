1fc34db6ba86057b22cb5cb5cbbea08912621e75
st1923 <santiagotoloza@hotmail.com>
2022-08-17 12:27:46 -0300

    agregado los iconos

---
+++ src/components/Clima.jsx
@@ -0,0 +1,10 @@
+import '../styles/Clima.css'
+
+const Clima = () => {
+  return (
+    <div>
+        <p>30gradelis</p>
+    </div>
+  )
+}
+export default Clima
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,35 @@
-import {rain,night,cloud,earth,afternoon,snow,sunset,wind,storm} from '../../src/img/index'
-import  '../styles/Header.css'
+import {
+  rain,
+  night,
+  cloud,
+  earth,
+  afternoon,
+  snow,
+  sunset,
+  wind,
+  storm,
+} from "../../src/img/index";
+import "../styles/Header.css";
 
-import {HiOutlineLocationMarker} from 'react-icons/hi'
+import Clima from "./Clima";
+import { HiOutlineLocationMarker } from "react-icons/hi";
+import { RiMenu5Fill } from "react-icons/ri";
 const Header = () => {
   return (
-    <div className='header'>
-        <img src={afternoon} alt="" className='rain' />
-        <HiOutlineLocationMarker/>
+    <div className="header">
+      <img src={afternoon} alt="" className="bg" />
+      <div className="main">
+        <div className="icons">
+          <div className="iconLeft">
+            <HiOutlineLocationMarker className="iconH" />
+            <p>Berisso</p>
+          </div>
+          <RiMenu5Fill className="iconH" />
+        </div>
+        <Clima/>
+
+      </div>
     </div>
-  )
-}
-export default Header
+  );
+};
+export default Header;
